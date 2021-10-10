980a553f2d47ec3bdf0ad0fde4bc824a7d794143
xiaolu <1324790234@qq.com>
2021-10-10 21:19:02 +0800

    修改路径

---
+++ chapter02/src/com/TestStack.java
@@ -0,0 +1,21 @@
+package com;
+
+public class TestStack {
+
+    public static void main(String[] args) {
+        int aa =1;
+        TestStack testStack = new TestStack();
+//        int bb = testStack.getInt();
+//        System.out.println(bb);
+    }
+     private void getInt() {
+        byte a= 1;
+         a++;
+        int b = 2;
+
+
+        ++b;
+
+
+     }
+}
